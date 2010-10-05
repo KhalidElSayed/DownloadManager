c01d6201c93ffc4c801aa2f134d1cfed6ef8211b
Carl Harroch <carl@novoda.com>
2010-10-05 12:20:32 +0100

    initial commit

---
+++ tests/src/novoda/net/dm/test/Landing.java
@@ -0,0 +1,15 @@
+package novoda.net.dm.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Landing extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
